242a242567bdb630f41d244e496c12cadfc088e9
Mathias Delusinne <mathias.itam@outlook.com>
2025-08-07 12:22:05 +0200

    Add ability to delete participant

---
+++ packages/evercamps/src/modules/camp/api/deleteParticipant/deleteParticipant.js
@@ -0,0 +1,23 @@
+import { INTERNAL_SERVER_ERROR, OK } from '../../../../lib/util/httpStatus.js';
+import deleteParticipant from '../../services/participant/deleteParticipant.js';
+
+export default async (request, response, next) => {
+  try {
+    const { id } = request.params;
+    const participant = await deleteParticipant(id, {
+      routeId: request.currentRoute.id
+    });
+    response.status(OK);
+    response.json({
+      data: participant
+    });
+  } catch (e) {
+    response.status(INTERNAL_SERVER_ERROR);
+    response.json({
+      error: {
+        status: INTERNAL_SERVER_ERROR,
+        message: e.message
+      }
+    });
+  }
+};
