d9a1eacaa6926262d873bca144ba3fea0cd1a37b
Janiek Colpaert <janiek.colpaert@gmail.com>
2025-08-27 23:12:49 +0200

    change to other package

--- packages/evercamps/src/modules/catalog/subscribers/product_image_added/generateLocalImages.js
+++ packages/evercamps/src/modules/catalog/subscribers/product_image_added/generateLocalImages.js
@@ -1,13 +1,63 @@
 import { error, debug } from '../../../../lib/log/logger.js';
 import { getConfig } from '../../../../lib/util/getConfig.js';
-import createLocalImages from '../../services/product/createLocalImages.js';
+import { existsSync } from 'fs';
+import path from 'path';
+import { update } from '@evershop/postgres-query-builder';
+import { CONSTANTS } from '../../../../lib/helpers.js';
+import { getConnection } from '../../../../lib/postgres/connection.js';
+import { Jimp } from 'jimp';
 
 export default async function generateLocalImages(data) {
   debug(`into generateLocalImages`);
   if (getConfig('system.file_storage') === 'local') {
     debug(`into local file storage: get config ${JSON.stringify(getConfig('system.file_storage'))}`);
     try {
-      await createLocalImages(data);
+      const imagePath = data.origin_image.replace('/assets', '');
+      debug(`image path: ${imagePath}`);
+      const mediaPath = path.join(CONSTANTS.MEDIAPATH, imagePath);
+      debug(`media path: ${mediaPath}`);
+      const ext = path.extname(path.resolve(CONSTANTS.MEDIAPATH, imagePath));
+      const singlePath = imagePath.replace(ext, `-single${ext}`);
+      const listingPath = imagePath.replace(ext, `-listing${ext}`);
+      const thumbnailPath = imagePath.replace(ext, `-thumb${ext}`);
+      if (existsSync(mediaPath)) {
+        debug(`Media path does exist`);
+        // Generate thumbnail
+        const image = await Jimp.read(mediaPath);
+        await image.resize({
+          w: getConfig('catalog.product.image.thumbnail.width', 100),
+          h: getConfig('catalog.product.image.thumbnail.height', 100)
+        });
+        await image.write(path.join(CONSTANTS.MEDIAPATH, thumbnailPath));
+
+        // Generate listing
+        await image
+          .resize({
+            w: getConfig('catalog.product.image.listing.width', 250),
+            h: getConfig('catalog.product.image.listing.height', 250)
+          });
+        await image.write(path.join(CONSTANTS.MEDIAPATH, listingPath));
+
+        // Generate single
+        await image
+          .resize({
+            w: getConfig('catalog.product.image.single.width', 500),
+            h: getConfig('catalog.product.image.single.height', 500)
+      });
+        await image.write(path.join(CONSTANTS.MEDIAPATH, singlePath));
+      }
+
+      // Update the record in the database with the new URLs in the variant columns
+      const connection = await getConnection();
+      await update('product_image')
+        .given({
+          single_image: `/assets${singlePath}`,
+          listing_image: `/assets${listingPath}`,
+          thumb_image: `/assets${thumbnailPath}`
+        })
+        .where('product_image_product_id', '=', data.product_image_product_id)
+        .and('origin_image', '=', data.origin_image)
+        .execute(connection);
     } catch (e) {
       error(e);
     }
