8d77cc6c4316691cfb7c3ebe705cb6d0bf6d9796
Janiek Colpaert <janiek.colpaert@gmail.com>
2025-08-27 14:27:09 +0200

    upload Full force

--- packages/evercamps/src/modules/catalog/subscribers/product_image_added/generateLocalImages.js
+++ packages/evercamps/src/modules/catalog/subscribers/product_image_added/generateLocalImages.js
@@ -1,10 +1,10 @@
 import { existsSync } from 'fs';
 import path from 'path';
-// import { update } from '@evershop/postgres-query-builder';
-// import sharp from 'sharp';
+import { update } from '@evershop/postgres-query-builder';
+import sharp from 'sharp';
 import { CONSTANTS } from '../../../../lib/helpers.js';
 import { error, debug } from '../../../../lib/log/logger.js';
-// import { pool } from '../../../../lib/postgres/connection.js';
+import { pool } from '../../../../lib/postgres/connection.js';
 import { getConfig } from '../../../../lib/util/getConfig.js';
 
 export default async function generateLocalImages(data) {
@@ -22,44 +22,44 @@ export default async function generateLocalImages(data) {
       const thumbnailPath = imagePath.replace(ext, `-thumb${ext}`);
       if (existsSync(mediaPath)) {
         debug(`Media path does exist`);
-        //       // Generate thumbnail
-        //       await sharp(mediaPath)
-        //         .resize(
-        //           getConfig('catalog.product.image.thumbnail.width', 100),
-        //           getConfig('catalog.product.image.thumbnail.height', 100),
-        //           { fit: 'inside' }
-        //         )
-        //         .toFile(path.join(CONSTANTS.MEDIAPATH, thumbnailPath));
+        // Generate thumbnail
+        await sharp(mediaPath)
+          .resize(
+            getConfig('catalog.product.image.thumbnail.width', 100),
+            getConfig('catalog.product.image.thumbnail.height', 100),
+            { fit: 'inside' }
+          )
+          .toFile(path.join(CONSTANTS.MEDIAPATH, thumbnailPath));
 
-        //       // Generate listing
-        //       await sharp(mediaPath)
-        //         .resize(
-        //           getConfig('catalog.product.image.listing.width', 250),
-        //           getConfig('catalog.product.image.listing.height', 250),
-        //           { fit: 'inside' }
-        //         )
-        //         .toFile(path.join(CONSTANTS.MEDIAPATH, listingPath));
+        // Generate listing
+        await sharp(mediaPath)
+          .resize(
+            getConfig('catalog.product.image.listing.width', 250),
+            getConfig('catalog.product.image.listing.height', 250),
+            { fit: 'inside' }
+          )
+          .toFile(path.join(CONSTANTS.MEDIAPATH, listingPath));
 
-        //       // Generate single
-        //       await sharp(mediaPath)
-        //         .resize(
-        //           getConfig('catalog.product.image.single.width', 500),
-        //           getConfig('catalog.product.image.single.height', 500),
-        //           { fit: 'inside' }
-        //         )
-        //         .toFile(path.join(CONSTANTS.MEDIAPATH, singlePath));
+        // Generate single
+        await sharp(mediaPath)
+          .resize(
+            getConfig('catalog.product.image.single.width', 500),
+            getConfig('catalog.product.image.single.height', 500),
+            { fit: 'inside' }
+          )
+          .toFile(path.join(CONSTANTS.MEDIAPATH, singlePath));
       }
 
-      //     // Update the record in the database with the new URLs in the variant columns
-      //     await update('product_image')
-      //       .given({
-      //         single_image: `/assets${singlePath}`,
-      //         listing_image: `/assets${listingPath}`,
-      //         thumb_image: `/assets${thumbnailPath}`
-      //       })
-      //       .where('product_image_product_id', '=', data.product_image_product_id)
-      //       .and('origin_image', '=', data.origin_image)
-      //       .execute(pool);
+      // Update the record in the database with the new URLs in the variant columns
+      await update('product_image')
+        .given({
+          single_image: `/assets${singlePath}`,
+          listing_image: `/assets${listingPath}`,
+          thumb_image: `/assets${thumbnailPath}`
+        })
+        .where('product_image_product_id', '=', data.product_image_product_id)
+        .and('origin_image', '=', data.origin_image)
+        .execute(pool);
     } catch (e) {
       error(e);
     }
