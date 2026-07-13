02a447bc00cac9a3e6097ffb0cfe767b1186119b
Janiek Colpaert <janiek.colpaert@gmail.com>
2026-07-13 17:33:08 +0200

    Modify constants

--- includes/lib/helpers.ts
+++ includes/lib/helpers.ts
@@ -4,20 +4,20 @@ import { getConfig } from './util/getConfig.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const rootPath = __dirname.includes(
-  path.join('node_modules', '@evercamps', 'evercamps')
-)
-  ? process.cwd()
-  : path.resolve(__dirname, '..', '..', '..', '..');
+
+const rootPath = path.resolve(__dirname, '..', '..');
 
 export const CONSTANTS = Object.freeze({
   ROOTPATH: rootPath,
-  LIBPATH: path.resolve(__dirname),
-  MODULESPATH: path.resolve(__dirname, '..', 'modules'),
-  PUBLICPATH: path.resolve(rootPath, 'public'),
+  LIBPATH: path.resolve(rootPath, 'includes', 'lib'),
+  COREPATH: path.resolve(rootPath, 'core'),
+  CONTENTPATH: path.resolve(rootPath, 'content'),
+  THEMEPATH: path.resolve(rootPath, 'content', 'theme'),
+  TRANSLATIONPATH: path.resolve(rootPath, 'content', 'translations'),
+  PLUGINPATH: path.resolve(rootPath, 'content', 'plugin'),
   MEDIAPATH: path.resolve(rootPath, 'content', 'media'),
+  PUBLICPATH: path.resolve(rootPath, 'public'),
   NODEMODULEPATH: path.resolve(rootPath, 'node_modules'),
-  THEMEPATH: path.resolve(rootPath, 'content', 'themes'),
   CACHEPATH: path.resolve(rootPath, 'evercamps'),
   BUILDPATH: path.resolve(rootPath, 'evercamps', 'build'),
   ADMIN_COLLECTION_SIZE: getConfig('admin_collection_size', 20)
