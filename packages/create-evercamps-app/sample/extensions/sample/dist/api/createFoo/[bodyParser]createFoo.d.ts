438b920ebfd6b5be131985d25155bd5e443c398c
Janiek Colpaert <janiek.colpaert@gmail.com>
2026-07-13 16:18:54 +0200

    closes #123

--- packages/create-evercamps-app/sample/extensions/sample/dist/api/createFoo/[bodyParser]createFoo.d.ts
+++
@@ -1,3 +0,0 @@
-import { EvercampsRequest, EvercampsResponse } from '@evercamps/evercamps';
-declare const _default: (request: EvercampsRequest, response: EvercampsResponse, next: any) => EvercampsResponse | undefined;
-export default _default;
