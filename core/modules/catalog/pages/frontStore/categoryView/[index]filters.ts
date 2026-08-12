c6f2dcfbf7b94ef01ea33016c37b9234dfe7498e
Mathias Delusinne <mathias.itam@outlook.com>
2026-08-12 13:39:45 +0200

    Fix error

--- core/modules/catalog/pages/frontStore/categoryView/[index]filters.ts
+++ core/modules/catalog/pages/frontStore/categoryView/[index]filters.ts
@@ -2,9 +2,11 @@ import { NextFunction } from 'express';
 import { buildFilterFromUrl } from '../../../../../lib/util/buildFilterFromUrl.js';
 import { EvercampsRequest } from '../../../../../types/request.js';
 import { setContextValue } from '../../../../graphql/services/contextHelper.js';
+import { EvercampsResponse } from '../../../../../types/response.js';
 
 export default async function (
   request: EvercampsRequest,
+  response: EvercampsResponse,
   next: NextFunction
 ): Promise<void> {
   setContextValue(request, 'filtersFromUrl', buildFilterFromUrl(request));
